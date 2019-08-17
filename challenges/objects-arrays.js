1118ec649ef31f227c3692b66709d4199b3e210e
Christopher Lien <cnlien@gmail.com>
2019-08-17 13:27:30 -0400

    Committing Arrays Final Code

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -31,44 +31,66 @@ let velociraptor = {
   length:'1.8m',
   period:'Late Cretaceous'
 }
-// Using your dinosaur objects, log answers to these questions:
 
-// How much did tyrannosaurus weigh?
+console.log(" ")
+console.log(" ")
+console.log("==== Objects ====")
+console.log("Using your dinosaur objects, log answers to these questions:")
+
+console.log(" ")
+console.log("How much did tyrannosaurus weigh?")
 console.log(tyrannosaurus.weight);
 
-// What was the diet of a velociraptor?
+console.log(" ")
+console.log("What was the diet of a velociraptor?")
 console.log(velociraptor.diet);
 
-// How long was a stegosaurus?
+console.log(" ")
+console.log("How long was a stegosaurus?")
 console.log(stegosaurus.length);
 
-// What time period did tyrannosaurus live in?
+console.log(" ")
+console.log("What time period did tyrannosaurus live in?")
 console.log(tyrannosaurus.period);
 
-
-// Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
+console.log(" ")
+console.log("Create a new roar method for the tyrannosaurus.  When called, return RAWERSRARARWERSARARARRRR! Log the result:")
 console.log(tyrannosaurus.roar);
 
 
 // ==== Arrays ====
 
 // Given an array of college graduates.  Complete the following requests using any array method you like
-
-const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
-{"id":2,"first_name":"Saundra","university":"The School of the Art Institute of Chicago","email":"swhal1@state.gov"},
-{"id":3,"first_name":"Lambert","university":"Marian College","email":"lparham2@techcrunch.com"},
-{"id":4,"first_name":"Modestine","university":"International Medical & Technological University","email":"mdolder3@symantec.com"},
-{"id":5,"first_name":"Chick","university":"Sultan Salahuddin Abdul Aziz Shah Polytechnic","email":"camorts4@google.com.au"},
-{"id":6,"first_name":"Jakob","university":"Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft","email":"jharken5@spiegel.de"},
-{"id":7,"first_name":"Robbi","university":"Salem University","email":"rbrister6@redcross.org"},
-{"id":8,"first_name":"Colline","university":"Coastal Carolina University","email":"cbrosh7@alibaba.com"},
-{"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
-{"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
+console.log(" ")
+console.log(" ")
+console.log('=== ARRAYS ===')
+console.log('Given an array of college graduates.  Complete the following requests using any array method you like')
+const graduates = [
+  {"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
+  {"id":2,"first_name":"Saundra","university":"The School of the Art Institute of Chicago","email":"swhal1@state.gov"},
+  {"id":3,"first_name":"Lambert","university":"Marian College","email":"lparham2@techcrunch.com"},
+  {"id":4,"first_name":"Modestine","university":"International Medical & Technological University","email":"mdolder3@symantec.com"},
+  {"id":5,"first_name":"Chick","university":"Sultan Salahuddin Abdul Aziz Shah Polytechnic","email":"camorts4@google.com.au"},
+  {"id":6,"first_name":"Jakob","university":"Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft","email":"jharken5@spiegel.de"},
+  {"id":7,"first_name":"Robbi","university":"Salem University","email":"rbrister6@redcross.org"},
+  {"id":8,"first_name":"Colline","university":"Coastal Carolina University","email":"cbrosh7@alibaba.com"},
+  {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
+  {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}
+]
 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+
+for (let i = 0; i<graduates.length; i++) {
+  universities.push(graduates[i].university)
+};
+
+universities.sort();
+
+console.log(" ")
+console.log('Request 1: Create a new array called universities that contains all the universities in the graduates array.')
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student.
@@ -78,19 +100,35 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+
+for (let i = 0; i<graduates.length; i++) {
+  contactInfo.push(graduates[i].first_name + " " + graduates[i].email);
+}
+
+console.log(" ")
+console.log("Request 2: Create a new array called contactInfo that contains both first name and email of each student.")
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+
+graduates.forEach(graduate => {
+  if (graduate.university.includes ("Uni"))
+  uni.push(graduate.university)
+})
+
+console.log(" ")
+console.log('Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result.')
 console.log(uni);
 
 
 // ==== ADVANCED Array Methods ====
 
 // Given this zoo data from around the United States, follow the instructions below.  Use the specific array methods in the requests below to solve the problems.
-
-
+console.log(" ")
+console.log(" ")
+console.log('=== ADVANCED Array Methods ===')
 zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":"Canis aureus","state":"Kentucky"},
 {"animal_name":"Screamer, southern","population":1,"scientific_name":"Chauna torquata","state":"Alabama"},
 {"animal_name":"White spoonbill","population":8,"scientific_name":"Platalea leucordia","state":"Georgia"},
@@ -102,12 +140,20 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 {"animal_name":"Hawk-eagle, crowned","population":10,"scientific_name":"Spizaetus coronatus","state":"Florida"},
 {"animal_name":"Australian pelican","population":5,"scientific_name":"Pelecanus conspicillatus","state":"West Virginia"}];
 
+
 /* Request 1: .forEach()
 
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
 const animalNames = [];
+
+zooAnimals.forEach(zooAnimals => {
+  animalNames.push(`Name: ${zooAnimals.animal_name}, Scientific: ${zooAnimals.scientific_name}`)
+})
+
+console.log(" ")
+console.log("Request 1: .forEach()")
 console.log(animalNames);
 
 /* Request 2: .map()
@@ -115,29 +161,49 @@ console.log(animalNames);
 The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 
 */
-
 const lowerCase = [];
-console.log(lowerCase);
 
+zooAnimals.map(zooAnimals => {
+  lowerCase.push(`Lower Case Name: ${zooAnimals.animal_name.toLowerCase()}`)
+});
+
+console.log(" ")
+console.log("Request 2: .map()")
+console.log(lowerCase);
+//
 /* Request 3: .filter()
 
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const lowerPopulation = [];
-console.log(lowerPopulation);
+let lowerPopulation = [];
 
+lowerPopulation = zooAnimals.filter(function(zooAnimals) {
+  return zooAnimals.population < 5;
+});
+
+console.log(" ")
+console.log("Request 3: .filter()")
+console.log(lowerPopulation);
+//
 /* Request 4: .reduce()
 
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
+let populationTotal = 0;
+
+populationTotal = zooAnimals.reduce ((totalAnimals, zooAnimals) => {
+  return totalAnimals + zooAnimals.population;
+}, 0);
+
+console.log(" ")
+console.log("Request 4: .reduce()")
+console.log(`Total Animals: ${populationTotal}`);
 
 
 /*
-
-Stretch: If you haven't already, convert your array method callbacks into arrow functions.
-
-*/
+//
+// Stretch: If you haven't already, convert your array method callbacks into arrow functions.
+//
+// */
